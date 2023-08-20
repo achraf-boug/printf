916c432438b939a0e803193f383ff69349847c9f
Parlog07 <127505288+Parlog07@users.noreply.github.com>
2023-08-20 20:40:20 +0100

    add check on format

--- _printf.c
+++ _printf.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * check_format - checks format for unhandled cases
+ * @format: string
+ * Return: 0 if OK, -1 if KO
+ */
+int check_format(char *format)
+{
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	return (0);
+}
+
+/**
  * _printf - function that produces output according to a format
  * @format: string to be printed
  * Return: output lenght
@@ -15,6 +29,9 @@ int _printf(const char *format, ...)
 	 */
 	int i, len = 0;
 	va_list argv;
+
+	if (check_format(format) == -1)
+		return (-1);
 
 	/*Let's get arguments using va_start*/
 	va_start(argv, format);
