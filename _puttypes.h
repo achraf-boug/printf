153110fa05332928d80be4a05034a80fe564d56e
achraf-boug <achraf.bougadir@gmail.com>
2023-08-21 23:54:24 +0100

    add Octal and Unsigned Int

--- _puttypes.h
+++ _puttypes.h
@@ -8,5 +8,7 @@ int _putchar(char c);
 int _putstr(char *str);
 int _putint(int number);
 int putBinary(unsigned int number);
+int putUnsignedInt(unsigned int number);
+int putOctal(unsigned int number);
 
 #endif
---
+++ _puttypes1.c
@@ -0,0 +1,41 @@
+#include "_puttypes.h"
+
+/**
+ * putUnsignedInt - prints Unsigned Int 
+ * @number: Unsigned Int
+ * Return: lenght of number
+ */
+int putUnsignedInt(unsigned int number)
+{
+	if (number < 0)
+		return (_putchar('-') + (-1 * number));
+	else if (number < 10)
+		return (_putchar(number + '0'));
+	else
+		return (putUnsignedInt(number / 10) + putUnsignedInt(number % 10));
+}
+
+/**
+ * putOctal - prints unsigned integer as octal
+ * @number: unsigned integer
+ * Return: lenght of octal
+*/
+int putOctal(unsigned int number)
+{
+	char digits[20];
+	int len = 0, i = 0;
+
+	if (number == 0)
+		return (_putchar('0'));
+	while (number > 0)
+	{
+		digits[i++] = '0' + (value & 7);
+		number = number >> 3;
+	}
+
+	while (i > 0)
+		len += _putchar(_putchar(digits[--i]));
+
+	return (len);
+}
+
