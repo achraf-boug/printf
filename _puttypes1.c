a5e415b1670087f54d51b450a2d9c80592a17820
Parlog07 <127505288+Parlog07@users.noreply.github.com>
2023-08-22 00:30:55 +0100

    "add putHexa"

--- _puttypes1.c
+++ _puttypes1.c
@@ -32,8 +32,37 @@ int putOctal(unsigned int number)
 	}
 
 	while (i > 0)
-		len += _putchar(_putchar(digits[--i]));
+		len += _putchar(digits[--i]);
 
 	return (len);
 }
 
+/**
+ * putHexa - prints unsigned integer as Hexa
+ * @number: unsigned integer
+ * @isUpper: flag for %x or %X
+ * Return: lenght of hexa
+*/
+int putHexa(unsigned int number, int isUpper)
+{
+	char digits[20];
+	int len = 0, i = 0;
+	int digit;
+
+	if (number == 0)
+		return (_putchar('0'));
+	while (number > 0)
+	{
+		digit = value & 0xF;
+		if (digit < 10)
+			digits[i++] = '0' + digit;
+		else
+			digits[i++] = (isUpper ? 'A' : 'a') + (digit - 10);
+		number = number >> 4;
+	}
+
+	while (i > 0)
+		len += _putchar(digits[--i]);
+
+	return (len);
+}
