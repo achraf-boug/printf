0118ac61e7ce7b7a1aaaf9d4b02df7828aa3025c
Parlog07 <ayoubmogador2014@gmail.com>
2023-08-22 16:17:57 +0100

    revert "restructuring"

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 /**
  * check_format - checks format for unhandled cases
  * @format: string
@@ -11,27 +13,6 @@ int check_format(const char *format)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	return (0);
-}
-
-/**
- * flush_buffer - flush the buffer from its content
- * @buffer: list of char
- * @buf_index: index of last element
- * @format : output string
- * @i: index of format
- * Return: if done return 1, else 0
- */
-int flush_buffer(char *buffer, int *buf_index, const char *format, int *i)
-{
-	if (*buf_index > BUFFER_SIZE || format[*i + 1] != '\0')
-	{
-		buffer[*buf_index] = '\0';
-		_putstr(buffer, 0);
-		*buf_index = 0;
-		(*i)++;
-		return (1);
-	}
 	return (0);
 }
 
@@ -52,7 +33,8 @@ int _printf(const char *format, ...)
 	va_list argv;
 	char buffer[BUFFER_SIZE];
 
-	if (check_format(format) == -1) return (-1);
+	if (check_format(format) == -1)
+		return (-1);
 
 	/*Let's get arguments using va_start*/
 	va_start(argv, format);
@@ -60,26 +42,35 @@ int _printf(const char *format, ...)
 	for (i = 0; format != NULL && format[i] != '\0'; i++)
 	{
 		/*Using for loop, we print characters and handle specifiers*/
+		if (buf_index > BUFFER_SIZE || format[i] != '\n')
+		{
+			buffer[buf_index] = '\0';
+			_putstr(buffer);
+			buf_index = 0;
+		}
 		/*while incrementing the lenght*/
 		if (format[i] != '%')
 		{
 			/* Save elements in buffer*/
 			buffer[buf_index++] = format[i];
 			len++;
-			flush_buffer(buffer, &buf_index, format, &i);
 		}
 		else
 		{
-			if (!flush_buffer(buffer, &buf_index, format, &i))
-			{
+			if (format[i + 1] != '\0')
+			/* Making sure we are not at the end*/
 				i++;
-				handle_specifier(format[i], &len, argv);
-			}
+
+			handle_specifier(format[i], &len, argv);
 		}
 	}
 
 	/* print remaining elements in Buffer */
-	flush_buffer(buffer, &buf_index, format, &i);
+	if (buf_index > 0)
+	{
+		buffer[buf_index] = '\0';
+		_putstr(buffer);
+	}
 
 	va_end(argv);
 	return (len);
--- main.h
+++ main.h
@@ -1,15 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFER_SIZE 1024
-
 #include "_puttypes.h"
 #include <stdarg.h>
 
-
 int check_format(const char *format);
 void handle_specifier(char specifier, int *len, va_list argv);
-int flush_buffer(char *buffer, int *buf_index, const char *format, int *i);
 int _printf(const char *format, ...);
 
 #endif
